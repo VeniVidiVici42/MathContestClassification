3b07c575a543c5c664d0b324f05028f353b2727e
Alexander Katz <bogtro123@gmail.com>
2019-06-05 22:21:08 -0400

    remove old version

--- main.py
+++ main.py
@@ -1,66 +1,45 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-import time
-import pickle
+from chrome_browser import ChromeBrowser
+import constants
+import utils
 
-aops_contests_url = "https://artofproblemsolving.com/community/c13_contests"
-aops_regional_contests_url = "https://artofproblemsolving.com/community/c16_national_and_regional_contests"
-contest_discussion_url = "https://artofproblemsolving.com/community/c40244_contest_collections_discussion"
-chrome_browser = webdriver.Chrome(ChromeDriverManager().install())
+chrome_browser = ChromeBrowser()
 
-def get_html():
-	return browser.execute_script("return document.body.innerHTML")
-
-visited_url = {}
-visited_url[contest_discussion_url] = True
-url_stack = [aops_regional_contests_url, contest_discussion_url]
+is_url_visited = {}
+is_url_visited[constants.contest_discussion_url] = True
+url_stack = [constants.aops_regional_contests_url, constants.contest_discussion_url]
 problems = []
-idx = 18
-
-def infiniscroll(browser):
-	for i in range(5):
-		browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		time.sleep(0.5)
+file_number = 1
 
 while len(url_stack) > 0:
+	# Retrieve a URL and update visited map accordingly
 	url = url_stack.pop()
-	if url in visited_url:
+	if url in is_url_visited:
 		continue
 	print(url)
-	visited_url[url] = True
-	chrome_browser.get(url)
-	infiniscroll(chrome_browser)
-	new_collections = chrome_browser.find_elements_by_class_name('cmty-full-cell-link')
-	new_urls = [collection.get_attribute('href') for collection in new_collections]
-	#print(new_urls)
+	is_url_visited[url] = True
+
+	chrome_browser.open_url(url)
+
+	# Elements are dynamically loaded on scroll, so scroll through the entire page to make sure we get everything
+	chrome_browser.infiniscroll()
+
+	# If URL points to a collection of contests (and/or other collections), get them all
+	new_collections = chrome_browser.get_collections()
+	new_urls = chrome_browser.get_collection_urls()
+
 	if len(new_urls) > 1:
-		# more collections
+		# More collections to visit later
 		url_stack = url_stack + new_urls
 	else:
-		# this is a contest
-		contest_problems = chrome_browser.find_elements_by_class_name('cmty-view-post-item-text')
+		# This is a contest page; collect the problems instead
+		contest_problems = chrome_browser.get_problems()
 		for contest_problem in contest_problems:
-			soup = BeautifulSoup(contest_problem.get_attribute('innerHTML'), 'html.parser')
-			for img in soup.findAll('img'):
-				img.replace_with(img['alt'])
-			problems.append(soup.text)
-			#print(soup.text)
-			#print()
-			#print()
-			if len(problems) % 10 == 0:
-				f = open('problems{0}pickle'.format(idx), 'wb')
-				g = open('problems{0}.txt'.format(idx), 'w', encoding='utf-8')
-				g.write('\n---------------\n'.join(problems))
-				g.close()
-				pickle.dump(problems, f)
-				f.close()
-				problems = []
-				idx = idx + 1
+			problems.append(contest_problem.get_text())
 
-f = open('problems{0}pickle'.format(idx), 'wb')
-g = open('problems{0}.txt'.format(idx), 'w', encoding='utf-8')
-g.write('\n---------------\n'.join(problems))
-g.close()
-pickle.dump(problems, f)
-f.close()
+			# Weird things can happen if we store too many problems at once (memory issues?). Dump them occasionally.
+			if len(problems) % constants.max_problems_per_file == 0:
+				utils.dump_problems(problems, file_number)
+				problems = []
+				file_number = file_number + 1
+
+utils.dump_problems(problems, file_number)
